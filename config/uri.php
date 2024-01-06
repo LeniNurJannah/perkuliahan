56db72f3deb4580a17c7a0aed090fe22ee83fc06
LeniNurJannah <leninurjannah2107@gmail.com>
2024-01-06 22:55:02 +0700

    first commit

---
+++ config/uri.php
@@ -0,0 +1,7 @@
+<?php
+
+function base_url() 
+{
+    $base_url = "http://localhost:8080/github/aplikasi_pendataan_dosen_dan_mahasiswa/"; // disesuaikan sesuai direktori root dokumen
+    return $base_url;
+}
